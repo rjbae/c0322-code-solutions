1fb9c455debb819569c434560127f1e095b3d188
Ryan Bae <baeryan21@gmail.com>
2022-03-21 18:02:43 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Ryan Bae';
+console.log('value of fullName:', fullName);
+var isCool = true;
+console.log('value of isCool:', isCool);
+var totalPets = 0;
+console.log('value of totalPets', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log(unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log(never);
+console.log('typeof never:', typeof never);
