16ec914a4b371bcfc82558701932868288a05f0e
Ryan Bae <baeryan21@gmail.com>
2022-03-26 00:01:16 +0000

    string-manipulation-1 in progress.

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,4 @@
+/* exported capitalize */
+function capitalize(word) {
+
+}
---
+++ string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,10 @@
+/* exported getLastChar */
+function getLastChar(string) {
+  return string[string.length - 1];
+}
+
+// Define a function named getLastChar with one parameter of string.
+// Trying to find the last character of a string.
+// In order to find the last character we must access the last index of the string.
+// To access the last index of the string, we have to subtract the length of the string by 1.
+// Return the value of the string.
