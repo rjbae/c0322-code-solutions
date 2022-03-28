09d0d03988473759c8a24547f35143c4f04704a2
Ryan Bae <baeryan21@gmail.com>
2022-03-28 17:59:51 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var heading = document.querySelector('h1');
+console.log('$heading', heading);
+console.dir(heading);
+
+var explanation = document.querySelector('#explanation');
+console.log('$explanation', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('$hint', hint);
+console.dir(hint);
+
+var text = document.querySelectorAll('p');
+console.log('$paragraphs', text);
+
+var link = document.querySelectorAll('.example-link');
+console.log('$links', link);
