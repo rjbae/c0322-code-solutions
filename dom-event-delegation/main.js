362525bfb5406baca0aa8930f507221f4e3f3447
Ryan Bae <baeryan21@gmail.com>
2022-04-02 00:22:19 +0000

    Applied changes to dom-event-delegation.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -6,10 +6,10 @@ taskDelegation.addEventListener('click', task);
 
 function task(event) {
   if (event.target && event.target.tagName === 'BUTTON') {
-    var element = document.querySelector('li');
+    var element = document.querySelector('button');
     var ancestor = element.closest('.task-list-item');
-    element.remove('button');
-    console.log(ancestor);
+    ancestor.remove();
+    console.log('closest .task-list-item', ancestor);
     console.log('event.target:', event.target);
     console.log('event.target.tagName:', event.target.tagName);
   }
