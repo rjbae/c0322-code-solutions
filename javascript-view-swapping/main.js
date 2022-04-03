0de7f501a7cfa5841556e949bfb03d54831920fc
Ryan Bae <baeryan21@gmail.com>
2022-04-03 19:09:50 +0000

    Completed javascript-view-swapping exercise.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,6 +1,6 @@
 var tabContainer = document.querySelector('.tab-container');
 var tabNodes = document.querySelectorAll('.tab');
-// var viewNodes = document.querySelectorAll('.view');
+var viewNodes = document.querySelectorAll('.view');
 
 tabContainer.addEventListener('click', tabEvent);
 
@@ -8,12 +8,20 @@ function tabEvent(event) {
 
   if (!event.target.matches('.tab')) return;
 
-  var tabIndex;
-  for (tabIndex = 0; tabIndex < tabNodes.length; tabIndex++) {
-    if (tabNodes === event.target) {
-      tabNodes[tabIndex].className = 'tab active';
+  for (var i = 0; i < tabNodes.length; i++) {
+    if (tabNodes[i] === event.target) {
+      tabNodes[i].className = 'tab active';
     } else {
-      tabNodes[tabIndex].className = 'tab';
+      tabNodes[i].className = 'tab';
+    }
+  }
+  var attribute = event.target.getAttribute('data-view');
+  for (var j = 0; j < viewNodes.length; j++) {
+    var viewNodeAttribute = viewNodes[j].getAttribute('data-view');
+    if (viewNodeAttribute === attribute) {
+      viewNodes[j].className = 'view';
+    } else {
+      viewNodes[j].className = 'hidden';
     }
   }
 }
