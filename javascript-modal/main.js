1d46d970f126efd314cf0d15f7f3c198f3b8789f
Ryan Bae <baeryan21@gmail.com>
2022-04-03 23:01:50 +0000

    Made changes to javascript-modal.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,24 +1,17 @@
-var popUp = true;
-
 var openButton = document.querySelector('.open-button');
+var closeButton = document.querySelector('.no-button');
 var modal = document.querySelector('.modal');
-var noButton = document.querySelector('.no-button');
 var background = document.querySelector('.background');
 
-openButton.addEventListener('click', modalEvent);
-noButton.addEventListener('click', modalEvent);
+openButton.addEventListener('click', openEvent);
+closeButton.addEventListener('click', closeEvent);
 
-function modalEvent(event) {
-  var show = null;
-  var noshow = null;
-  if (popUp === true) {
-    popUp = false;
-    noshow = 'noshow';
-  } else {
-    popUp = true;
-    show = 'show';
-  }
-  modal.className = 'modal ' + noshow;
-  openButton.className = 'open-button ' + show;
-  background.className = 'background ' + show;
+function openEvent(event) {
+  modal.className = 'modal';
+  background.className = 'background';
+}
+
+function closeEvent(event) {
+  modal.className = 'no-show';
+  background.classList.remove('background');
 }
