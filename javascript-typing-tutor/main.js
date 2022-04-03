ffc741076833f190edf058ee09b73f5ed0d4ac0f
Ryan Bae <baeryan21@gmail.com>
2022-04-03 05:46:37 +0000

    js typing tutor in progress.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,16 @@
-var char = document.querySelectorAll('span');
+// var charSpan = document.querySelectorAll('span');
+// var characters = document.querySelector('.letters');
+// var correct = true;
 
-char.addEventListener('keydown', charEvent);
-
-function charEvent(event) {
-
-}
+// document.addEventListener('keydown', function (event) {
+//   for (var i = 0; i < charSpan.length; i++) {
+//     var text = charSpan[i].innerText;
+//     if (event.key === charSpan[i].innerText) {
+//       correct = true;
+//       charSpan.className = 'correct';
+//     } else {
+//       correct = false;
+//       charSpan.className = 'incorrect';
+//     }
+//   }
+// });
